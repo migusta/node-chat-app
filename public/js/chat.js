93d5fea86895f70d7793190550e37ac984e57a7f
Migusta <amigushina@gmail.com>
2018-03-06 12:20:56 +0300

    Add users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,16 +17,31 @@ function scrollToBottom(){
 }
 
 socket.on('connect', function (){
-  console.log("Connect to server");
+  var params = jQuery.deparam(window.location.search);
 
-  // socket.emit('createMessage', {
-  //   from: 'Andrew',
-  //   text: 'How r u?'
-  // })
+  socket.emit('join', params, function(err){
+    if(err) {
+      alert(err);
+      window.location.href='/';
+    }
+    else{
+      console.log('No error');
+    }
+  })
 });
 
 socket.on('disconnect', function () {
   console.log("Diconnected from server");
+});
+
+socket.on("updateUserList", function(users) {
+  var ol = jQuery('<ol></ol>');
+  console.log(users);
+  users.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery("#users").html(ol);
 });
 
 socket.on('newMessage', function(message) {
